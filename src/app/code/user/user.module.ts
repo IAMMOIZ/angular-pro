ded68d0df5514fc4cc54bb6c2da94418582512f4
IAMMOIZ <moizmit02@gmail.com>
2020-07-05 16:33:14 +0530

    1 file modified, 4 added, 4 deleted

--- src/app/code/user/user.module.ts
+++ src/app/code/user/user.module.ts
@@ -1,1 +1,7 @@
-import { }
+import { NgModule } from '@angular/core';
+import { RouterModule , Routes} from '@angular/router';
+import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
+
+@NgModule({
+  imports : [ FormsModule, ReactiveFormsModule , RouterModule.forChild()]
+})
