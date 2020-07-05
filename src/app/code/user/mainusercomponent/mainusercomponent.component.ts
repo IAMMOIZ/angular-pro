bae98a067ec66c82b2cea629f4fe2d16e212b6ce
IAMMOIZ <moizmit02@gmail.com>
2020-07-05 17:12:52 +0530

    5 files added, 5 deleted

---
+++ src/app/code/user/mainusercomponent/mainusercomponent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'idm-mainusercomponent',
+  // templateUrl: './mainusercomponent.component.html',
+  // styleUrls: ['./mainusercomponent.component.css']
+})
+export class MainUserComponentComponent {
+  constructor()
+  {
+    console.log("this is user component");
+  }
+}
---
+++ src/app/code/user/usermodule.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule , Routes} from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
+// import { RouterModule , Routes } from '@angular/router';
+// import { MainusercomponentComponent } from './mainusercomponent/mainusercomponent.component';
+import { MainUserComponentComponent } from './mainusercomponent/mainusercomponent.component';
+import { UsermoduleRoutingModule } from './usermodule.router';
+const routes : Routes = [{path : '' , component : MainUserComponentComponent }]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    UsermoduleRoutingModule
+    ]
+
+})
+export class UsermoduleModule
+{
+  constructor(){
+    console.log("user module is rendering");
+  }
+}
