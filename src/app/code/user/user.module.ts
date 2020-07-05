a551e34a0b90f6f36875c6c5faeac237773e6c65
IAMMOIZ <moizmit02@gmail.com>
2020-07-05 16:37:43 +0530

    1 file modified

--- src/app/code/user/user.module.ts
+++ src/app/code/user/user.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule , Routes} from '@angular/router';
 import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
-
+import { RouterModule , Routes } from '@angular/router';
+import { MainUserComponet } from './mainuser/mainuser.component'
+const routes : Routes = [{path : '' , component : MainUserComponent }]
 @NgModule({
-  imports : [ FormsModule, ReactiveFormsModule , RouterModule.forChild()]
+declarations : [ MainUserComponent],
+  imports : [ FormsModule, ReactiveFormsModule , RouterModule.forChild(routes)],
+exports : [ MainUserComponent]
 })
