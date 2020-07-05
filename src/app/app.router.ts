9d9b00b8d55d771fe34c9620beaba86575e0b99d
IAMMOIZ <moizmit02@gmail.com>
2020-07-05 16:49:48 +0530

    3 files modified

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -3,12 +3,12 @@ import { Routes } from '@angular/router';
 // import components and features modules
 import { HomeComponent } from './home/home.component';
 import { SearchModule } from './code/search/search.module';
-
-
+import { UserModule } from './code/user/user.module';
 
 
 export const routes : Routes = [
 // { path : '' , component : AppComponent},
 { path : 'home' , component : HomeComponent},
-{ path : 'search' , loadChildren : './code/search/search.module#SearchModule'}
+{ path : 'search' , loadChildren : './code/search/search.module#SearchModule'},
+{ path : 'user' , loadChildren : './code/user/user.module#UserModule'}
 ]
--- src/app/code/user/user.module.ts
+++ src/app/code/user/user.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule , Routes} from '@angular/router';
 import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
-import { RouterModule , Routes } from '@angular/router';
-import { MainUserComponet } from './mainuser/mainuser.component'
+// import { RouterModule , Routes } from '@angular/router';
+import { MainUserComponent } from './mainuser/mainuser.component'
 const routes : Routes = [{path : '' , component : MainUserComponent }]
 @NgModule({
 declarations : [ MainUserComponent],
   imports : [ FormsModule, ReactiveFormsModule , RouterModule.forChild(routes)],
 exports : [ MainUserComponent]
 })
+
+export class UserModule
+{
+  constructor(){
+    console.log("user module is rendering");
+  }
+}
